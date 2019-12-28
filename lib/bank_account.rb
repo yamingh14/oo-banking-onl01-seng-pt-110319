2e9d6c4504f2b9411e981e903dca2ac86fb734e7
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 15:41:03 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,64 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
+
+# class BankAccount
+#   attr_reader :name
+#   attr_accessor :balance, :status, :account_hash
+#
+#   def initialize(name)
+#     @name = name
+#     @balance = 1000
+#     @status = "open"
+#   end
+#
+#   def deposit(deposit_amount)
+#     @balance += deposit_amount
+#   end
+#
+#   def display_balance
+#     p "Your balance is $#{self.balance}."
+#   end
+#
+#   def status=(status)
+#     @status
+#   end
+#
+#   def balance=(balance)
+#     @balance
+#   end
+#
+#   def valid?
+#     (!!status == 'open') || (!!balance > 0)
+#   end
+#
+# def account_hash
+#     instance_variables.map do |var|
+#         [var[1..-1].to_sym, instance_variable_get(var)]
+#     end.to_h
+#   end
+#
+# end
