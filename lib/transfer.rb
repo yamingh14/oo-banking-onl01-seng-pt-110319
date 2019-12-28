36e9bd2631cd9d75d04c2ad0a20518d96447959e
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-28 16:39:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
+      @status = "closed"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
